a1628dbee4463c91faf12826859ef0b501a55370
zacck <coderv63@gmail.com>
2017-03-23 21:24:29 +0200

    work on checking weather and reporting

---
+++ example-callback.js
@@ -0,0 +1,30 @@
+var request = require('request');
+const key = 'b3c17071c6f0bf5ed04f42571244404a';
+var baseUrl = `http://api.openweathermap.org/data/2.5/weather?`
+
+var authU = `${baseUrl}APPID=${key}`;
+
+//console.log(authU);
+
+var city = 'capetown'
+
+var cityUrl = `${authU}&q=${city}`
+
+var units = 'metric'
+
+var unitUrl = `${cityUrl}&units=${units}`
+
+//console.log(cityUrl);
+
+request({url: unitUrl, json: true}, function(error, response, body ) {
+  if(error) {
+    console.log(error.message, 'occured')
+  } else {
+    const name  = body.name;
+    const weather = body.weather[0].main;
+    const high = body.main.temp_max;
+    const low = body.main.temp_min;
+    //console.log(JSON.stringify(body, null, 4))
+    console.log(`${name} weather today is ${weather} with highs of ${high} celcius and lows of ${low} celcius`)
+  }
+});
