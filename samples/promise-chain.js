18e5dfc5a1c1b03b54007727da617704c214a066
zacck <coderv63@gmail.com>
2017-03-27 12:18:19 +0200

    use catch to find error in promise chain

---
+++ samples/promise-chain.js
@@ -0,0 +1,20 @@
+function doWork(shouldFail) {
+  return new  Promise(function(resolve, reject) {
+    if(shouldFail) {
+      reject
+    }
+    setTimeout(() => {
+      console.log('done');
+      resolve();
+    }, 1000)
+  });
+}
+
+
+doWork().then(() => {
+  return doWork(shouldFail)
+}).then(() => {
+  console.log('all done')
+}).catch((err) => { //this will be run if any of our promises does not resolve
+  console.log('caught error somewhere');
+})
