278de0d9f21a2c38b0c3ca23d313c71543b21861
zacck <coderv63@gmail.com>
2017-03-29 14:58:53 +0200

    work on showing direction

--- weather.js
+++ weather.js
@@ -9,6 +9,33 @@ module.exports = (location = '') => {
   var city;
   var cityUrl;
 
+  const getDir = (angle) => {
+    //easy to customize by changing the number of directions you have
+        var directions = 8;
+
+        var degree = 360 / directions;
+        angle = angle + degree/2;
+
+        if (angle >= 0 * degree && angle < 1 * degree)
+            return "N";
+        if (angle >= 1 * degree && angle < 2 * degree)
+            return "NE";
+        if (angle >= 2 * degree && angle < 3 * degree)
+            return "E";
+        if (angle >= 3 * degree && angle < 4 * degree)
+            return "SE";
+        if (angle >= 4 * degree && angle < 5 * degree)
+            return "S";
+        if (angle >= 5 * degree && angle < 6 * degree)
+            return "SW";
+        if (angle >= 6 * degree && angle < 7 * degree)
+            return "W";
+        if (angle >= 7 * degree && angle < 8 * degree)
+            return "NW";
+        //Should never happen:
+        return "N";
+  }
+
   //console.log(cityUrl);
 
   const weatherGetter =  () => {
@@ -22,11 +49,12 @@ module.exports = (location = '') => {
          const high = body.main.temp_max;
          const low = body.main.temp_min;
          //console.log(JSON.stringify(body, null, 4))
-         const windKms = body.wind.speed * 3.6
+         const windKms = body.wind.speed * 3.6;
+         const dir = getDir(body.wind.deg);
          if(windKms < 34) {
            console.log(` Winds are ok for Surfing`);
          }
-         resolve(`\n ${name} weather today is ${weather}  \n Highs of ${high} celcius and lows of ${low} celcius  \n Wind: speed -> ${windKms} km/h \n`)
+         resolve(`\n ${name} weather today is ${weather}  \n Highs of ${high} celcius and lows of ${low} celcius  \n Wind \n speed -> ${windKms} km/h, Direction: -> ${dir} \n`)
        }
      });
     });
